a5a891afa7b1da7a0568a8c38a5b7c141b8a9f6d
Lynn Orrell <lyorrell@shadowbook2.localdomain>
2019-12-10 05:34:52 -0600

    Updated rating function

--- CreateRatingFunction.cs
+++ CreateRatingFunction.cs
@@ -32,15 +32,17 @@ namespace microsoft.gbb
                 return new BadRequestObjectResult("UserId is not valid.");
             }
 
-            // string userIdRequest = $"https://serverlessohuser.trafficmanager.net/api/GetUser?userId={model.userId}";
-            // var response = await _httpClient.GetAsync(userIdRequest);
-            // if(!response.IsSuccessStatusCode)
-            // {
-            //     return new BadRequestObjectResult("UserId is not valid.");
-            // }
+            string productIdRequest = $"https://serverlessohproduct.trafficmanager.net/api/GetProduct?productId={model.productId}";
+            response = await _httpClient.GetAsync(productIdRequest);
+            if(!response.IsSuccessStatusCode)
+            {
+                return new BadRequestObjectResult("ProductId is not valid.");
+            }
 
+            model.id = Guid.NewGuid().ToString();
+            model.timestamp = DateTime.UtcNow;
 
-            return new OkResult();
+            return new CreatedResult($"http://localhost:7071/api/Ratings/{model.id}", model);
         }
     }
 }
