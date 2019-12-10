e9dd859241aec5be8c1e4c52681fe59afe1d88d3
Joey Schluchter <jschluchter@gmail.com>
2019-12-10 14:34:52 -0800

    update to create

--- GetRatingFunction.cs
+++ GetRatingFunction.cs
@@ -1,17 +1,14 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System.Net.Http;
 
 namespace microsoft.gbb
 {
-	public static class GetRatingFunction
+    public static class GetRatingFunction
 	{
 		private static HttpClient _httpClient = new HttpClient();
 
