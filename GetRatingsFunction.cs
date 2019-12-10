1b9535bddfa0165b92afe69b850804868ba58164
Joey Schluchter <jschluchter@gmail.com>
2019-12-10 13:07:46 -0800

    moving to a more reliable repo

---
+++ GetRatingsFunction.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace microsoft.gbb
+{
+    public static class GetRatingsFunction
+    {
+        private static HttpClient _httpClient = new HttpClient();
+
+        [FunctionName("GetRatings")]
+        public static async Task<IActionResult> GetRatings(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Ratings")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
---
+++ RatingModel.cs
@@ -0,0 +1,13 @@
+namespace microsoft.gbb
+{
+    public class RatingModel
+    {
+        public string id { get; set; }
+        public string userId { get; set; }
+        public string productId { get; set; }
+        public string timestamp { get; set; }
+        public string locationName { get; set; }
+        public int rating { get; set; }
+        public string userNotes { get; set; }
+    }
+}
